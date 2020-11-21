673d9c2c9fadd6fe70a279d4647e9adcb95e531d
Pavel <mirprost@gmail.com>
2020-11-21 12:32:54 +0300

    lesson-1

---
+++ lesson-1/less1_task2.js
@@ -0,0 +1,9 @@
+/*
+Работа с переменными.
+Объявить две переменные: admin и name. Записать в name строку "Василий"; Скопировать значение из name в admin. Вывести admin (должно вывести «Василий»).
+*/
+
+"use strict";
+let name = "Василий";
+let admin = name;
+document.getElementById("lesson2").innerHTML = admin;
